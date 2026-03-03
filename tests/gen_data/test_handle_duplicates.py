7c5db61deee285d898be543ced78ce2e3750f91b
michal-ry <ryziomichal@gmail.com>
2026-03-03 17:21:36 +0100

    Add ValueError test for duplicate detection in handle_duplicates

--- tests/gen_data/test_handle_duplicates.py
+++ tests/gen_data/test_handle_duplicates.py
@@ -1,7 +1,5 @@
 import pandas as pd
 from data_toolkit.cleaning import handle_duplicates
-import sys
-print(sys.path)
 
 def test_no_df_input_error():
 
@@ -15,5 +13,22 @@ def test_no_df_input_error():
         raise AssertionError('TypeError was not raised for non-DataFrame input')
     
 
+def test_dupplicates_error():
+
+    df = pd.DataFrame({
+    "id": [1, 2, 2],
+    "value": ["A", "B", "B"]
+})
+    
+    try:
+        handle_duplicates(df, action='raise')
+    except ValueError:
+        pass
+    else:
+        raise AssertionError("ValueError was not raise for duplicated DataFrame.")
+
+
 # Test 1: TypeError - non-DataFrame input
 test_no_df_input_error()
+# Test 2: ValueError - duplicated DataFrame
+test_dupplicates_error()
